48e2f62c98edd55b3ab17083ccfcdc82a87bcd01
Getu Taddesse <Getu_Taddesse@mckinsey.com>
2023-02-26 21:34:43 -0500

    first commit

---
+++ ppmtoolFullStack/src/main/java/com/get/ppmtoolFullStack/PpmtoolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.get.ppmtoolFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolFullStackApplication.class, args);
+	}
+
+}
---
+++ ppmtoolFullStack/src/test/java/com/get/ppmtoolFullStack/PpmtoolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.get.ppmtoolFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
